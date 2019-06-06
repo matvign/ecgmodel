242cdb82f684728bea35842f36e1ec1d43485cd6
Andrew Au <andrew.ch.au@outlook.com>
2019-06-06 13:02:00 +1000

    refactor into object oriented tk

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import tkinter as tk
 
 from matplotlib.backends.backend_tkagg import (
@@ -9,6 +10,100 @@ import numpy as np
 from scipy.integrate import solve_ivp
 
 pi = np.pi
+
+
+class MainApplication(tk.Frame):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.master.title('Embedding in Tk')
+        self.master.geometry('900x450')
+        self.master.option_add('*tearOff', 0)
+
+        self.content = tk.Frame(self.master)
+        self.ecgframe = tk.Frame(self.content, bd=4, relief='sunken', height=400, width=500)
+        self.formframe = tk.Frame(self.content)
+        self.buttonframe = tk.Frame(self.content)
+
+        self.content.grid(sticky='nsew')
+        self.ecgframe.grid(row=0, column=0, sticky='nsew', padx=(0, 10), pady=(10, 0))
+        self.formframe.grid(row=0, column=1, columnspan=1, sticky='nsew', pady=(10, 0))
+        self.buttonframe.grid(row=1, column=1, columnspan=1, sticky='nsew')
+
+        self.initmenu()
+        self.initform()
+
+        self.buildbtn = tk.Button(self.formframe, text='Build', command=self.build_ecg, pady=8)
+        self.buildbtn.grid(row=12, column=1, columnspan=2, pady=20)
+
+        figure = Figure(figsize=(5, 4), dpi=100)
+        self.fig = figure.add_subplot(111)
+        self.ecgcanvas = FigureCanvasTkAgg(figure, self.ecgframe)
+        self.ecgcanvas.get_tk_widget().grid()
+
+        toolbarframe = tk.Frame(self.content)
+        toolbarframe.grid(row=1, column=0, columnspan=12, sticky='w')
+        self.toolbar = NavigationToolbar2Tk(self.ecgcanvas, toolbarframe)
+        self.toolbar.update()
+
+    def initmenu(self):
+        self.menubar = tk.Menu(self.master)
+        filemenu = tk.Menu(self.menubar)
+        aboutmenu = tk.Menu(self.menubar)
+
+        filemenu.add_command(label='Exit', command=self.master.destroy)
+        aboutmenu.add_command(label='About')
+
+        self.menubar.add_cascade(label='File', menu=filemenu)
+        self.menubar.add_cascade(label='About', menu=aboutmenu)
+        self.master.config(menu=self.menubar)
+
+    def initform(self):
+        vals = {
+            "a": [1.2, -5.0, 30.0, -7.5, 0.75],
+            "b": [0.25, 0.1, 0.1, 0.1, 0.4],
+            "event": [-pi/3, -pi/12, 0, pi/12, pi/2]
+        }
+        a, b, events = [], [], []
+        for i in range(0, 5):
+            # create 5 labels and entries for a,b,event
+            lbl_a = tk.Label(self.formframe, text='a{}'.format(i+1))
+            lbl_b = tk.Label(self.formframe, text='b{}'.format(i+1))
+            lbl_event = tk.Label(self.formframe, text='theta{} (pi)'.format(i+1))
+
+            entry_a = tk.Entry(self.formframe, width=10)
+            entry_a.insert(0, str(vals["a"][i]))
+
+            entry_b = tk.Entry(self.formframe, width=10)
+            entry_b.insert(0, str(vals["b"][i]))
+
+            entry_event = tk.Entry(self.formframe, width=10)
+            entry_event.insert(0, str(vals["event"][i]))
+
+            a.append((lbl_a, entry_a))
+            b.append((lbl_b, entry_b))
+            events.append((lbl_event, entry_event))
+
+            for count, (lbl, entry) in enumerate(a+b+events):
+                # create right aligned labels and entries
+                col = 2 if count % 2 else 0
+                ro = int(count/2)
+                lbl.grid(row=ro, column=col, sticky='e')
+                entry.grid(row=ro, column=col+1)
+
+            # create label+entry for omega, the angular velocity
+            lbl_w = tk.Label(self.formframe, text='w (pi)').grid(row=8, column=0, sticky='e')
+            entry_w = tk.Entry(self.formframe, width=10)
+            entry_w.insert(0, str(2*pi))
+            entry_w.grid(row=8, column=1)
+
+    def build_ecg(self):
+        self.fig.cla()
+        sol = build_ecg()
+        self.fig.plot(sol.t, sol.y[2], 'b-')
+        self.ecgcanvas.draw()
 
 
 def odefcn(T, Y, a, b, w, events):
@@ -33,69 +128,6 @@ def odefcn(T, Y, a, b, w, events):
     return dy
 
 
-def initmenu(parent):
-    '''
-    Initialize menu components
-    Menubar
-      Filemenu
-        Exit
-      Aboutmenu
-        About
-    '''
-    menubar = tk.Menu(parent)
-
-    filemenu = tk.Menu(menubar)
-    aboutmenu = tk.Menu(menubar)
-
-    filemenu.add_command(label='Exit', command=parent.destroy)
-    aboutmenu.add_command(label='About')
-
-    menubar.add_cascade(label='File', menu=filemenu)
-    menubar.add_cascade(label='About', menu=aboutmenu)
-    parent.config(menu=menubar)
-
-
-def initform(parent):
-    vals = {
-        "a": [1.2, -5.0, 30.0, -7.5, 0.75],
-        "b": [0.25, 0.1, 0.1, 0.1, 0.4],
-        "event": [-pi/3, -pi/12, 0, pi/12, pi/2]
-    }
-
-    a, b, events = [], [], []
-    for i in range(0, 5):
-        # create 5 labels and entries for a,b,event
-        lbl_a = tk.Label(parent, text='a{}'.format(i+1))
-        lbl_b = tk.Label(parent, text='b{}'.format(i+1))
-        lbl_event = tk.Label(parent, text='theta{} (pi)'.format(i+1))
-
-        entry_a = tk.Entry(parent, width=10)
-        entry_a.insert(0, str(vals["a"][i]))
-
-        entry_b = tk.Entry(parent, width=10)
-        entry_b.insert(0, str(vals["b"][i]))
-
-        entry_event = tk.Entry(parent, width=10)
-        entry_event.insert(0, str(vals["event"][i]))
-
-        a.append((lbl_a, entry_a))
-        b.append((lbl_b, entry_b))
-        events.append((lbl_event, entry_event))
-
-    for count, (lbl, entry) in enumerate(a+b+events):
-        # create right aligned labels and entries
-        col = 2 if count % 2 else 0
-        ro = int(count/2)
-        lbl.grid(row=ro, column=col, sticky='e')
-        entry.grid(row=ro, column=col+1)
-
-    # create label+entry for omega, the angular velocity
-    lbl_w = tk.Label(parent, text='w (pi)').grid(row=8, column=0, sticky='e')
-    entry_w = tk.Entry(parent, width=10)
-    entry_w.insert(0, str(2*pi))
-    entry_w.grid(row=8, column=1)
-
-
 def build_ecg(a=None, b=None, evt=None, w=2*pi):
     if a is None:
         a = np.array([1.2, -5.0, 30.0, -7.5, 0.75])
@@ -111,54 +143,11 @@ def build_ecg(a=None, b=None, evt=None, w=2*pi):
     return sol
 
 
-def app():
-    def generatePlot():
-        ecgplot.cla()
-        sol = build_ecg()
-        ecgplot.plot(sol.t, sol.y[2], 'b-')
-        ecgcanvas.draw()
-
+def main():
     root = tk.Tk()
-
-    # remove tearoff from menus
-    root.option_add('*tearOff', 0)
-    root.wm_title("Embedding in Tk")
-    root.geometry('900x450')
-
-    initmenu(root)
-
-    # create frames and initialise their grids
-    content = tk.Frame(root)
-    ecgframe = tk.Frame(content, bd=4, relief='sunken', height=400, width=500)
-    formframe = tk.Frame(content)
-    buttonFrame = tk.Frame(content)
-
-    content.grid(sticky='nsew')
-    ecgframe.grid(row=0, column=0, sticky='nsew', padx=(0, 10), pady=(10, 0))
-    formframe.grid(row=0, column=1, columnspan=1, sticky='nsew', pady=(10, 0))
-    buttonFrame.grid(row=1, column=1, columnspan=1, sticky='nsew')
-
-    initform(formframe)
-    button = tk.Button(formframe, text='Build', command=generatePlot, pady=8)
-    button.grid(row=12, column=1, columnspan=2, pady=20)
-
-    # build ecg and display it in the ecgframe
-    fig = Figure(figsize=(5, 4), dpi=100)
-
-    ecgcanvas = FigureCanvasTkAgg(fig, ecgframe)
-    ecgcanvas.get_tk_widget().grid()
-
-    ecgplot = fig.add_subplot(111)
-    generatePlot()
-
-    # create a toolbar for the figure in the ecgframe
-    toolbarFrame = tk.Frame(root)
-    toolbarFrame.grid(row=1, column=0, columnspan=12, sticky='w')
-    toolbar = NavigationToolbar2Tk(ecgcanvas, toolbarFrame)
-    toolbar.update()
-
+    app = MainApplication()
     root.mainloop()
 
 
 if __name__ == "__main__":
-    app()
+    main()
