9ecbb1476c6d80b3df74bb9be866aaa3896fbd01
Andrew Au <andrew.ch.au@outlook.com>
2019-06-27 13:09:40 +1000

    add regex for coefficient replacement

--- helpers/helper.py
+++ helpers/helper.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+import re
+
+pattern = re.compile(r"(pi\s*|([0-9.]+)(?=(\s*pi|[0-9.])")
+
+
 def triway(lst):
     it = iter(lst)
     while True:
@@ -6,3 +11,12 @@ def triway(lst):
             yield next(it), next(it), next(it)
         except StopIteration:
             return
+
+
+def pirepl(matchobj):
+    if matchobj.group(1).strip() == 'pi':
+        return "{}*".format(matchobj.group(1))
+    elif matchobj.group(2).strip() == 'pi':
+        return "{}*".format(matchobj.group(1))
+    else:
+        return matchobj.group(0)
