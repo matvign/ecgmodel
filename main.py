9bb15f41f9cc9ff423fa2258925cbe577eb35032
Andrew Au <andrew.ch.au@outlook.com>
2019-07-16 17:40:05 +1000

    enable high dpi scaling

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import sys
+import PyQt5.QtCore as QtCore
 from PyQt5.QtWidgets import QApplication
 
 from gui import ecgapp
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
     ecgprog = ecgapp.ECGModel()
     sys.exit(app.exec())
