faa79ff5eed3b6f06d6bcdef23f4efc1f5751d5e
Andrew Au <andrew.ch.au@outlook.com>
2019-09-24 22:46:09 +1000

    add math for ecg/ekf/jacobian, add menu action for ekf

--- gui/ecgapp.py
+++ gui/ecgapp.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import numexpr as ne
 
-from numpy import array as nparr
 from matplotlib.backends.backend_qt5agg import FigureCanvas
 from matplotlib.backends.backend_qt5agg import (
     NavigationToolbar2QT as NavigationToolbar)
@@ -20,6 +19,13 @@ from helpers import helper
 
 
 class ECGModel(QMainWindow):
+    defaults = {
+        'a': ["1.2", "-5.0", "30.0", "-7.5", "0.75"],
+        'b': ["0.25", "0.1", "0.1", "0.1", "0.4"],
+        'evt': ["-pi/3", "-pi/12", "0", "pi/12", "pi/2"],
+        'omega': ["2*pi"]
+    }
+
     def __init__(self, parent=None):
         super().__init__()
 
@@ -37,7 +43,6 @@ class ECGModel(QMainWindow):
         self.show()
 
     def initUI(self):
-        self.initParams()
         self.initMenu()
 
         self._mainwidget = QWidget()
@@ -51,75 +56,61 @@ class ECGModel(QMainWindow):
         self.init_ecgframe()
         self.init_formframe()
 
-    def initParams(self):
-        self.exprdict = {'pi': helper.pi}
-        self.axparams = {
-            'axis': 'y',
-            'which': 'both',
-            'top': False, 'bottom': False,
-            'left': False, 'right': False,
-            'labeltop': False, 'labelbottom': False,
-            'labelleft': False, 'labelright': False,
-        }
-        self.preset = {
-            'defaults': {
-                'a': ["1.2", "-5.0", "30.0", "-7.5", "0.75"],
-                'b': ["0.25", "0.1", "0.1", "0.1", "0.4"],
-                'evt': ["-pi/3", "-pi/12", "0", "pi/12", "pi/2"],
-                'omega': ["2*pi"]
-            }
-        }
-
     def initMenu(self):
         menubar = self.menuBar()
 
         filemenu = menubar.addMenu('File')
 
         importmenu = QMenu('Import', self)
-        importparam = QAction('Import Parameters', self)
-        importparam.setShortcut('Ctrl+O')
-        importparam.triggered.connect(self.importParams)
+        importParam = QAction('Import Parameters', self)
+        importParam.setShortcut('Ctrl+O')
+        importParam.triggered.connect(self.import_params)
+        importmenu.addAction(importParam)
 
-        importsample = QAction('Import Sample', self)
-        importsample.setShortcut('Ctrl+Shift+O')
-        importsample.setStatusTip('Import sample ECG data')
-        importsample.triggered.connect(self.importSample)
-        importmenu.addAction(importparam)
-        importmenu.addAction(importsample)
+        importSample = QAction('Import Sample', self)
+        importSample.setShortcut('Ctrl+Shift+O')
+        importSample.setStatusTip('Import sample ECG data')
+        importSample.triggered.connect(self.import_sample)
+        importmenu.addAction(importSample)
+
+        filemenu.addMenu(importmenu)
 
         exportAction = QAction('Export', self)
         exportAction.setShortcut('Ctrl+S')
         exportAction.setStatusTip('Export current parameters')
-        exportAction.triggered.connect(lambda: self.exportParams())
+        exportAction.triggered.connect(lambda: self.export_params())
+        filemenu.addAction(exportAction)
+
+        ekfAction = QAction('EKF', self)
+        ekfAction.setStatusTip('Generate estimate from ECG sample')
+        ekfAction.triggered.connect(self.buildEKF)
+        filemenu.addAction(ekfAction)
 
         resetAction = QAction('Reset', self)
         resetAction.setShortcut('Ctrl+R')
         resetAction.setStatusTip('Reset current parameters')
-        resetAction.triggered.connect(self.defaultParams)
+        resetAction.triggered.connect(self.set_defaults)
+        filemenu.addAction(resetAction)
 
         clearSample = QAction('Clear Sample', self)
         clearSample.setStatusTip('Remove ECG sample')
-        clearSample.triggered.connect(self.removeSample)
+        clearSample.triggered.connect(lambda: self.removePlot('sample'))
+        filemenu.addAction(clearSample)
 
         clearEstimate = QAction('Clear Estimate', self)
         clearEstimate.setStatusTip('Remove ECG estimate')
-        clearEstimate.triggered.connect(self.removeEst)
+        clearEstimate.triggered.connect(lambda: self.removePlot())
+        filemenu.addAction(clearEstimate)
 
         clearAll = QAction('Clear All', self)
         clearAll.setStatusTip('Clear all graphs')
         clearAll.triggered.connect(self.removeAll)
+        filemenu.addAction(clearAll)
 
         exitAction = QAction('Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
-
-        filemenu.addMenu(importmenu)
-        filemenu.addAction(exportAction)
-        filemenu.addAction(resetAction)
-        filemenu.addAction(clearSample)
-        filemenu.addAction(clearEstimate)
-        filemenu.addAction(clearAll)
         filemenu.addAction(exitAction)
 
         aboutmenu = menubar.addMenu('About')
@@ -127,16 +118,9 @@ class ECGModel(QMainWindow):
     def init_ecgframe(self):
         self.fig = Figure(figsize=(5, 3))
         self.fig.suptitle('Electrocardiogram Simulation')
-        self.ax1 = self.fig.add_subplot(111, label='estimate')
-        self.ax1.set_xlabel('time (s)')
-        self.ax1.set_ylabel('mV')
-
-        # self.ax2 = self.ax1.twinx()
-        # self.ax2.tick_params(**self.axparams)
-        # self.fig.add_axes(self.ax2, label='sample')
-
-        print(self.fig.get_axes())
-        print(self.fig.gca())
+        self.ax = self.fig.add_subplot(111, label='estimate')
+        self.ax.set_xlabel('time (s)')
+        self.ax.set_ylabel('mV')
 
         ecgframe = FigureCanvas(self.fig)
         self._grid.addWidget(ecgframe, 0, 0)
@@ -156,27 +140,25 @@ class ECGModel(QMainWindow):
         dbl_validator = QDoubleValidator(decimals=8)
         dbl_validator.setNotation(0)
 
-        defaults = self.preset['defaults']
-
         entries_a = QHBoxLayout()
         entries_b = QHBoxLayout()
         entries_evt = QHBoxLayout()
         for i in range(5):
             entry_a = make_entry(dbl_validator)
-            entry_a.insert(str(defaults['a'][i]))
+            entry_a.insert(str(ECGModel.defaults['a'][i]))
 
             entry_b = make_entry(dbl_validator)
-            entry_b.insert(str(defaults['b'][i]))
+            entry_b.insert(str(ECGModel.defaults['b'][i]))
 
             entry_evt = make_entry(pi_validator)
-            entry_evt.insert(defaults['evt'][i])
+            entry_evt.insert(ECGModel.defaults['evt'][i])
 
             entries_a.addWidget(entry_a)
             entries_b.addWidget(entry_b)
             entries_evt.addWidget(entry_evt)
 
         entry_omega = make_entry(pi_validator)
-        entry_omega.insert(defaults['omega'][0])
+        entry_omega.insert(ECGModel.defaults['omega'][0])
 
         # create a form
         self.formlayout = QFormLayout()
@@ -251,11 +233,10 @@ class ECGModel(QMainWindow):
         msg.setText('Build Error: could not generate ECG, check for invalid parameters')
         msg.exec()
 
-    def defaultParams(self):
-        data = self.preset['defaults']
-        self.set_entries(data)
+    def set_defaults(self):
+        self.set_entries(ECGModel.defaults)
 
-    def importParams(self):
+    def import_params(self):
         caption = 'Import Parameters'
         f_filter = 'JSON (*.json)'
         path = ''
@@ -274,7 +255,17 @@ class ECGModel(QMainWindow):
         self.set_entries(data)
         self.buildECG(a, b, evt, omega)
 
-    def importSample(self, filename, timeframe=1.5):
+    def export_params(self, filename='ecgdata.json'):
+        caption = 'Export Parameters'
+        path = 'ecgdata.json'
+        f_filter = 'JSON (*.json)'
+        fileName = QFileDialog.getSaveFileName(self, caption, path, f_filter)
+        if not fileName[0]:
+            return
+        a, b, evt, omega = self.get_entries()
+        helper.export_json(fileName[0], a, b, evt, omega)
+
+    def import_sample(self, filename, timeframe=1):
         caption = 'Import ECG Sample'
         f_filter = 'CSV (*.csv)'
         path = ''
@@ -290,65 +281,39 @@ class ECGModel(QMainWindow):
 
         data = helper.filter_timeframe(samples, tframe)
 
-        self.removeSample()
-        self.ax1.plot(data[0:, 0], data[0:, 1], 'r--', label='sample')
-        self.recalc_axis()
-        self.recalc_legend()
-        self.ax1.figure.canvas.draw()
+        self.removePlot('sample')
+        self.ax.plot(data[0:, 0], data[0:, 1], 'r--', label='sample')
+        self.redraw_axes()
 
-    def exportParams(self, filename='ecgdata.json'):
-        caption = 'Export Parameters'
-        path = 'ecgdata.json'
-        f_filter = 'JSON (*.json)'
-        fileName = QFileDialog.getSaveFileName(self, caption, path, f_filter)
-        if not fileName[0]:
-            return
-        a, b, evt, omega = self.get_entries()
-        helper.export_json(fileName[0], a, b, evt, omega)
-
-    def recalc_legend(self):
-        ax1_hndl, ax1_lbl = self.ax1.get_legend_handles_labels()
-        if self.ax1.get_legend():
-            self.ax1.get_legend().remove()
-            if not ax1_lbl:
-                return
-
-        self.ax1.legend(handles=ax1_hndl, labels=ax1_lbl)
-
-    def recalc_axis(self):
-        self.ax1.relim()
-        self.ax1.autoscale_view()
-
-    def removeEst(self):
-        for l in self.ax1.get_lines():
-            if l.get_label() == 'estimate':
+    def removePlot(self, ln='estimate'):
+        for l in self.ax.get_lines():
+            if l.get_label() == ln:
                 l.remove()
-        self.recalc_axis()
-        self.recalc_legend()
-        self.ax1.figure.canvas.draw()
-
-    def removeSample(self):
-        for l in self.ax1.get_lines():
-            if l.get_label() == 'sample':
-                l.remove()
-        self.recalc_axis()
-        self.recalc_legend()
-        self.ax1.figure.canvas.draw()
+            self.redraw_axes()
 
     def removeAll(self):
-        for l in self.ax1.get_lines():
+        for l in self.ax.get_lines():
             l.remove()
-        self.recalc_axis()
-        self.recalc_legend()
-        self.ax1.figure.canvas.draw()
+        self.redraw_axes()
 
     def parseParams(self, a, b, evt, omega):
-        arr_a = nparr([float(i) for i in a])
-        arr_b = nparr([float(i) for i in b])
-        arr_evt = nparr([ne.evaluate(helper.pirepl(i), self.exprdict) for i in evt])
-        f_omega = ne.evaluate(helper.pirepl(omega), self.exprdict)
+        arr_a = [float(i) for i in a]
+        arr_b = [float(i) for i in b]
+        arr_evt = [ne.evaluate(helper.pirepl(i), {'pi': helper.pi}) for i in evt]
+        f_omega = ne.evaluate(helper.pirepl(omega), {'pi': helper.pi})
 
         return (arr_a, arr_b, arr_evt, f_omega)
+
+    def redraw_axes(self):
+        self.ax.relim()
+        self.ax.autoscale_view()
+
+        axe_hndl, axe_lbl = self.ax.get_legend_handles_labels()
+        if self.ax.get_legend():
+            self.ax.get_legend().remove()
+        if axe_lbl:
+            self.ax.legend(handles=axe_hndl, labels=axe_lbl)
+        self.ax.figure.canvas.draw()
 
     def buildParams(self):
         try:
@@ -359,9 +324,36 @@ class ECGModel(QMainWindow):
         self.buildECG(arr_a, arr_b, arr_evt, f_omega)
 
     def buildECG(self, a, b, evt, omega):
-        self.removeEst()
+        self.removePlot()
         sol = helper.solve_ecg(a, b, evt, omega)
-        self.ax1.plot(sol.t, sol.y[2], 'b-', label='estimate')
-        self.recalc_axis()
-        self.recalc_legend()
-        self.ax1.figure.canvas.draw()
+        self.ax.plot(sol.t, sol.y[2], 'b-', label='estimate')
+        self.redraw_axes()
+
+    def buildEKF(self):
+        """
+        Y (observations) = amplitudes taken from samples
+        X_k (initial state) = [-1, 0, 0]
+        P_k (covariance matrix) = [...]
+
+        Q_k (measurement noise) = [...]
+        R_k (process noise) = [1]
+
+        a, b, evt (constants)
+        omega (angular velocity) = approximation based on length of sample
+        """
+        sample = next((l for l in self.ax.get_lines() if l.get_label() == 'sample'), None)
+        if not sample:
+            print('no sample found!')
+            return
+        Y = sample.get_ydata()
+        X = [-1, 0, 0]
+        P = 10
+        Q = 0
+        R = 10
+        a = [float(i) for i in ECGModel.defaults['a']]
+        b = [float(i) for i in ECGModel.defaults['b']]
+        evt = [ne.evaluate(helper.pirepl(i), {'pi': helper.pi}) for i in ECGModel.defaults['evt']]
+        w = sample.get_xdata()[-1] * 2 * helper.pi
+
+        res = helper.solve_ekf(Y, X, P, Q, R, a, b, evt, w)
+        print(res)
--- helpers/helper.py
+++ helpers/helper.py
@@ -53,39 +53,154 @@ def filter_timeframe(data, timeframe):
     return data[data[:, 0] < timeframe]
 
 
-def solve_ecg(a=None, b=None, evt=None, w=2*np.pi):
-    def odefcn(T, Y, a, b, w, events):
-        '''
-        Function to solve ODE with scipy.integrate.solve_ivp()
-        Details located here (p291):
-        http://web.mit.edu/~gari/www/papers/ieeetbe50p289.pdf
-        '''
-        x, y, z = Y
-        dy = np.zeros(3)
+def solve_ecg(a, b, evt, w=2*np.pi):
+    arr_a = np.asarray(a, dtype="float")
+    arr_b = np.asarray(b, dtype="float")
+    arr_evt = np.asarray(evt, dtype="float")
 
-        theta = np.arctan2(y, x)
-        alpha = 1.0 - np.sqrt(x**2 + y**2)
-        dy[0] = alpha*x - w*y
-        dy[1] = alpha*y + w*x
-
-        dy[2] = -(z - 0)
-        for i in range(0, 5):
-            dtheta = theta - events[i]
-            dy[2] -= a[i] * dtheta * np.exp(-(dtheta**2) / (2 * b[i]**2))
-
-        return dy
-
-    if a is None:
-        a = np.array([1.2, -5.0, 30.0, -7.5, 0.75])
-    if b is None:
-        b = np.array([0.25, 0.1, 0.1, 0.1, 0.4])
-    if evt is None:
-        evt = np.array([-np.pi/3, -np.pi/12, 0, np.pi/12, np.pi/2])
-
-    tspan = np.array([-1.0, 1.0])
-    y0 = np.array([-1.0, 0.0, 0.0])
+    tspan = np.array([0, 1], dtype="float")
+    y0 = np.array([-1.0, 0.0, 0.0], dtype="float")
     teval = np.linspace(0, 1, num=100)
     print('building...')
-    sol = solve_ivp(fun=lambda t, y: odefcn(t, y, a, b, w, evt),
+    sol = solve_ivp(fun=lambda t, y: ecg_model(y, a, b, evt, w),
                     t_span=tspan, y0=y0, t_eval=teval)
     return sol
+
+
+def ecg_model(X, a, b, evt, w=2*np.pi, z0=0):
+    '''
+    Function to solve ODE with scipy.integrate.solve_ivp()
+    Details located here (p291):
+    http://web.mit.edu/~gari/www/papers/ieeetbe50p289.pdf
+    '''
+    x, y, z = X
+    dX = np.zeros(3)
+
+    theta = np.arctan2(y, x)
+    alpha = 1.0 - np.sqrt(x**2 + y**2)
+    dX[0] = alpha*x - w*y
+    dX[1] = alpha*y + w*x
+
+    dX[2] = -(z - z0)
+    for i in range(0, 5):
+        dtheta = theta - evt[i]
+        dX[2] -= a[i] * dtheta * np.exp(-(dtheta**2) / (2 * b[i]**2))
+
+    return dX
+
+
+def solve_ekf(Y, X, P, Q, R, a, b, evt, w):
+    '''
+    Y: measurement/observations
+    X: state matrix         P: covariance matrix
+    Q: measurement noise    R: process noise
+
+    a: constant             b: constant
+    evt: constant           w: angular velocity
+
+    Run Kalman Filter until we're out of observations.
+    Predict: run predict with the updated state
+    Update: create a new update with the estimate
+    '''
+    Y = np.asarray(Y, dtype="float")
+    X, P = np.asmatrix(X, dtype="float").T, np.asmatrix(np.eye(3)*P, dtype="float")
+    Q, R = np.asmatrix(Q, dtype="float"), np.asmatrix(np.eye(3)*R, dtype="float")
+
+    a, b = np.asarray(a, dtype="float"), np.asarray(b, dtype="float")
+    evt = np.asarray(evt, dtype="float")
+
+    prior_X, prior_P = None, None
+    post_X, post_P = X, P
+    res = [(post_X, post_P)]
+
+    for obs in Y:
+        prior_X, prior_P = predict(post_X, post_P, Q, a, b, evt, w)
+        post_X, post_P = update(prior_X, prior_P, R, obs)
+        res.append(post_X)
+
+    return res
+
+
+def ecg_jacobian(X, a, b, evt, w):
+    '''
+    Linearized version of ecg_model wrt x, y, z
+    '''
+    x, y, z = np.array(X)[0]
+    sqrt_xy = np.sqrt(x**2 + y**2)
+    theta = np.arctan2(y, x)
+
+    dF_x = 1 + (-2*x**2 - y**2)/sqrt_xy
+    dF_y = -w + (-x*y)/sqrt_xy
+    dF_z = 0
+
+    dG_x = w + (-x*y)/sqrt_xy
+    dG_y = 1 + (-2*y**2 - x**2)/sqrt_xy
+    dG_z = 0
+
+    dH_x = 0
+    dH_y = 0
+    dH_z = -1
+
+    for i in range(0, 5):
+        dtheta = theta - evt[i]
+        component = np.exp(-dtheta**2/2*b**2) * (1 - dtheta**2/b**2)
+        dH_x += component * (a[i]*y)/(x**2 + y**2)
+        dH_y += component * (-a[i]*x)/(x**2 + y**2)
+
+    return np.matrix([
+        [dF_x, dF_y, dF_z],
+        [dG_x, dG_y, dG_z],
+        [dH_x, dH_y, dH_z]
+    ])
+
+
+def predict(X, P, Q, a, b, evt, w):
+    '''
+    X: state matrix         P: covariance matrix
+    Q: process noise
+    A: linearized ecg_model wrt to X
+    F: linearized ecg_model wrt to noise
+
+    a: constant             b: constant
+    evt: constant           w: angular velocity
+
+    Predict step:
+    prior_X = f(x, w, k)
+    prior_P = A_k * post_P * A_k^T + F_k * Q_k * F_k^T
+    '''
+    prior_X = ecg_model(X, a, b, evt, w)
+
+    A = ecg_jacobian(X, a, b, evt, w)
+    F = np.matrix([0, 0, 0], dtype="float").T
+    prior_P = A*P*A.T + F*Q*F.T
+
+    return (prior_X, prior_P)
+
+
+def update(prior_X, prior_P, R, Y):
+    '''
+          Y: observation/measurements (amplitudes)
+    prior_X: estimate to be updated with observation
+     post_X: updated estimate of state with measurements
+    prior_P: estimate of the covariance matrix
+     post_P: updated estimate of covariance with measurements
+          K: Kalman gain
+          g: [0 0 1]X + v_k
+          C: dg/dX
+          G: dg/dv
+
+    Update step:
+         K = prior_P * C^T [C * prior_P * C^T + G]^(-1)
+    post_X = prior_X + K[Y - g(x, 0, k)]
+    post_P = prior_P - K * C * prior_P
+    '''
+    g = np.matrix([0, 0, 1], dtype="float") * prior_X.T + 0
+    C = np.matrix([0, 0, 1], dtype="float")
+    G = np.matrix([1], dtype="float")
+
+    S = C * prior_P * C.T + G
+    K = prior_P*C.T * S.I
+
+    post_X = prior_X + K * (Y - g)
+    post_P = prior_P - K * C * prior_P
+    return (post_X, post_P)
