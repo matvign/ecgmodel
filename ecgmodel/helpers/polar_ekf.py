ef45bdb30e9d9cdc487e50be5d52a76505a38442
Andrew Au <andrew.ch.au@outlook.com>
2019-10-27 14:42:28 +1100

    rename functions, experimental parameter fitting

--- ecgmodel/helpers/polar_ekf.py
+++ ecgmodel/helpers/polar_ekf.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 
-def polar_discrete_model(X, a, b, evt, dt, omega=2*np.pi, N=0):
+def discrete_model(X, a, b, evt, dt, omega=2*np.pi, N=0):
     theta, z = X
     dX = np.zeros(2)
     dtheta = [(theta - ei) for ei in evt]
@@ -15,28 +15,30 @@ def polar_discrete_model(X, a, b, evt, dt, omega=2*np.pi, N=0):
     return dX
 
 
-def polar_state_jacobian(X, a, b, evt, dt, omega=2*np.pi):
-    """Polar ECG model jacobian wrt state variables theta, z """
-    theta, z = X
+def discrete_model_alt(X, dt, omega=2*np.pi, N=0):
+    """Alternate version of discrete model where state
+    X = [theta, z, a1, .., b1, .., evt1, .., evt5]
+    """
+    theta, z = X[0:2]
+    a = X[2:7]
+    b = X[7:12]
+    evt = X[12:17]
     dtheta = [(theta - ei) for ei in evt]
 
-    dF_theta = 1
-    dF_z = 0
-
-    dG_theta = -sum((
-        dt * omega * (ai/bi**2) * (1 - (dthi**2/bi**2)) * np.exp(-(dthi**2)/2*bi**2))
+    Xk = np.zeros(17)
+    Xk[0] = theta + omega*dt
+    Xk[1] = z + (N * dt) - sum(
+        dt * omega * ai * dthi * np.exp(-(dthi**2)/(2*bi**2))
         for ai, bi, dthi in zip(a, b, dtheta)
     )
-    dG_z = 1
-    return np.matrix([
-        [dF_theta, dF_z],
-        [dG_theta, dG_z]
-    ])
+    Xk[2:17] = [*a, *b, *evt]
+    return Xk
 
 
-def polar_state_jacobian2(X, a, b, evt, omega, dt):
-    """Polar ECG model jacobian wrt state variables
+def state_jacobian(X, a, b, evt, dt, omega):
+    """Function to return jacobian of polar ECG model wrt state vector
     X = [theta, z, a1, .., a5, b1, .., b5, theta1, .., theta5, omega, N]
+
     """
     theta, z = X
     dtheta = [(theta - ei) for ei in evt]
@@ -54,7 +56,7 @@ def polar_state_jacobian2(X, a, b, evt, omega, dt):
         for ai, bi, dthi in zip(a, b, dtheta)
     ]
     dG_bk = [
-        -2 * dt * (ai/bi**3) * omega * (1 - (dthi**2/2*bi**2)) * np.exp(-(dthi**2)/(2*bi**2))
+        -2 * dt * (ai/bi**3) * omega * dthi * (1 - (dthi**2/2*bi**2)) * np.exp(-(dthi**2)/(2*bi**2))
         for ai, bi, dthi in zip(a, b, dtheta)
     ]
     dG_ek = [
@@ -69,40 +71,91 @@ def polar_state_jacobian2(X, a, b, evt, omega, dt):
     return A
 
 
-def polar_noise_jacobian(X, a, b, evt, dt, omega=2*np.pi, N=0):
-    """Polar ECG model jacobian wrt process noise
-    wk = [a1, .., a5, b1, .., b5, theta1, .., theta5, omega, N]
+def state_jacobian_alt(X, dt, omega):
+    """Alternative to state_jacobian() """
+    theta, z = X[0:2]
+    a = X[2:7]
+    b = X[7:12]
+    evt = X[12:17]
+    dtheta = [(theta - ei) for ei in evt]
+
+    dF_theta = 1
+    dF_z = 0
+
+    dG_theta = -sum((
+        dt * (ai/bi**2) * omega * (1 - (dthi**2/bi**2)) * np.exp(-(dthi**2)/2*bi**2))
+        for ai, bi, dthi in zip(a, b, dtheta)
+    )
+    dG_z = 1
+    dG_ak = [
+        -dt * (omega/bi**2) * dthi * np.exp(-(dthi**2)/(2*bi**2))
+        for ai, bi, dthi in zip(a, b, dtheta)
+    ]
+    dG_bk = [
+        -2 * dt * (ai/bi**3) * omega * dthi * (1 - (dthi**2/2*bi**2)) * np.exp(-(dthi**2)/(2*bi**2))
+        for ai, bi, dthi in zip(a, b, dtheta)
+    ]
+    dG_ek = [
+        dt * (ai/bi**2) * omega * (1 - (dthi**2/2*bi**2)) * np.exp(-(dthi**2)/(2*bi**2))
+        for ai, bi, dthi in zip(a, b, dtheta)
+    ]
+    A = np.asmatrix(np.eye(17), dtype="float")
+    A[0, :2] = [dF_theta, dF_z]
+    A[1, :] = [dG_theta, dG_z, *dG_ak, *dG_bk, *dG_ek]
+    for i in range(2, 17):
+        A[i, i] = 1
+    return A
+
+
+def noise_jacobian(X, a, b, evt, dt, omega=2*np.pi, N=0):
+    """Function to return jacobian of polar ECG model wrt noise vector
+    wk = [omega, n, u1, .., u15]
+
     """
     theta, z = X
     dtheta = [(theta - ei) for ei in evt]
 
-    dF_ak = [0 for i in range(0, 5)]
-    dF_bk = [0 for i in range(0, 5)]
-    dF_ek = [0 for i in range(0, 5)]
     dF_omega = dt
     dF_n = 0
 
-    dG_ak = [
-        -dt * omega * (dthi/bi**2) * np.exp(-(dthi**2)/(2*bi**2))
-        for ai, bi, dthi in zip(a, b, dtheta)
-    ]
-    dG_bk = [
-        2 * dt * omega * dthi * (ai/bi**3) * np.exp(-(dthi**2)/(2*bi**2))
-        for ai, bi, dthi in zip(a, b, dtheta)
-    ]
-    dG_ek = [
-        dt * omega * (ai/bi**2) * (1 - (dthi**2)/b**2) * np.exp(-(dthi**2)/(2*bi**2))
-        for ai, bi, dthi in zip(a, b, dtheta)
-    ]
     dG_omega = -sum((
-        dt * dthi * (ai/bi**2) * np.exp(-(dthi**2)/(2*bi**2)))
+        dt * (ai/bi**2) * dthi * np.exp(-(dthi**2)/(2*bi**2)))
         for ai, bi, dthi in zip(a, b, dtheta)
     )
     dG_n = 1
-    return np.matrix([
-        [*dF_ak, *dF_bk, *dF_ek, dF_omega, dF_n],
-        [*dG_ak, *dG_bk, *dG_ek, dG_omega, dG_n]
-    ])
+
+    F = np.asmatrix(np.zeros(17, 17))
+    F[0, :2] = [dF_omega, dF_n]
+    for i in range(2, 17):
+        F[i, i] = 1
+    return F
+
+
+def noise_jacobian_alt(X, dt, omega=2*np.pi, N=0):
+    """Function to return jacobian of polar ECG model wrt noise vector
+    wk = [omega, n, u1, .., u15]
+
+    """
+    theta, z = X[0:2]
+    a = X[2:7]
+    b = X[7:12]
+    evt = X[12:17]
+    dtheta = [(theta - ei) for ei in evt]
+
+    dF_omega = dt
+    dF_n = 0
+
+    dG_omega = -sum((
+        dt * (ai/bi**2) * dthi * np.exp(-(dthi**2)/(2*bi**2)))
+        for ai, bi, dthi in zip(a, b, dtheta)
+    )
+    dG_n = 1
+
+    F = np.asmatrix(np.zeros(17, 17))
+    F[0, :2] = [dF_omega, dF_n]
+    for i in range(2, 17):
+        F[i, i] = 1
+    return F
 
 
 def solve_ecg(a, b, evt, omega=2*np.pi):
@@ -118,7 +171,7 @@ def solve_ecg(a, b, evt, omega=2*np.pi):
     xs = []
 
     while tk < tf:
-        xk = polar_discrete_model(xk, a, b, evt, dt, omega)
+        xk = discrete_model(xk, a, b, evt, dt, omega)
         xs.append(xk)
 
         ts.append(tk)
@@ -128,38 +181,43 @@ def solve_ecg(a, b, evt, omega=2*np.pi):
 
 
 def solve_ecg_ekf(ys, ts, a, b, evt, omega):
-    """Run Kalman Filter
+    """Parameter fit sample ECG with Extended Kalman Filter
 
-    ys: observations
-    ts: the timeframes
-    xk: vector for the system state
-    pk: covariance matrix of the system
+    x: state vector
+    p: covariance matrix
     w: process noise vector
     v: measurement noise vector
 
-    f(): ecg model
-    g(): update function
-    A: jacobian of system of equations wrt x
-    F: jacobian of system of equations wrt w
-    C: jacobian of update equations wrt x
-    G: jacobian of update equations wrt v
-    Q: process noise matrix, equal to F * Q * F.T
-    R: measurement noise matrix, equal to G * R * G.T
+    Q: variance of process noise
+    R: variance of measurement noise
+    Qk: covariance matrix of process noise w/variance Q
+    Rk: covariance matrix of measurement noise w/variance R
 
-    r: innovation signal
-    k: kalman gain
+    f: system propagation model
+    A: jacobian of system wrt state vector x
+    F: jacobian of system wrt noise vector w
 
-    a: values that only change during update step
-    b: values that only change during update step
-    evt: values that only change during update step
-    omega: angular velocity, fixed in our case
+    g: state-measurement model
+    C: jacobian of state-measurement wrt state vector x
+    G: jacobian of state-measurement wrt noise vector v
 
     """
-    xk = np.asarray([-np.pi/3, 0], dtype="float")
-    pk = np.asmatrix(np.eye(2), dtype="float")
+    xk = np.asarray([-np.pi, 0, *a, *b, *evt], dtype="float")
+    pk = np.asmatrix(np.eye(17), dtype="float")
 
-    Q = np.asmatrix(np.eye(17), dtype="float")
-    jacobian_f = polar_discrete_model()
+    Q = float(1)
+    Qk = np.asmatrix(np.eye(17)*Q, dtype="float")
+
+    R = float(1)
+    Rk = np.asmatrix(np.eye(1)*R, dtype="float")
+
+    # f = discrete_model_alt
+    # A = state_jacobian_alt
+    # F = noise_jacobian
+
+    g = np.asarray([0, 1, *[0 for i in range(15)]])
+    # C = np.asmatrix([0, 1])
+    # G = np.asmatrix([1])
 
     a = np.asarray(a, dtype="float")
     b = np.asarray(b, dtype="float")
@@ -171,16 +229,21 @@ def solve_ecg_ekf(ys, ts, a, b, evt, omega):
     for tk, yk in zip(ts, ys):
         dt = tk - t_old
 
-        # perform state prediction
-        # x_hat = ecg_discrete_model(xk, dt, a, b, evt, omega)
-        x_hat = ecg_discrete_polar(xk, dt, a, b, evt, omega)
-
-        # perform covariance prediction
-        # p_hat = ecg_predict(jacobian_f, dt, xk, pk, Q, a, b, evt, omega)
-        p_hat = ecg_predict2(jacobian_f, xk, dt, pk, Q, dt, a, b, evt)
+        # perform state and covariance prediction
+        x_hat = discrete_model_alt(xk, dt, omega)
+        Ak = state_jacobian_alt(xk, dt, omega)
+        Fk = noise_jacobian_alt(xk, dt, omega)
+        p_hat = Ak * pk * Ak.T + Fk * Qk * Fk.T
 
         # perform state update
-        xk, pk = ecg_update2(yk, dt, x_hat, p_hat)
+        C = np.asmatrix([0, 1], dtype="float")
+        G = np.asmatrix([1], dtype="float")
+        K = p_hat * C.T * (C * p_hat * C.T + G.T)
+
+        xk = x_hat + K * (yk - g*x_hat)
+        xk = np.asarray(xk.T)[0]
+        pk = p_hat - K*C*p_hat
+
         xs.append(xk)
 
         # update last time
@@ -188,30 +251,3 @@ def solve_ecg_ekf(ys, ts, a, b, evt, omega):
 
     # return (ts, [i[2] for i in xs[:]])
     return (ts)
-
-
-def ecg_predict(jacobian, X, dt, P, Q, a, b, evt):
-    """Function for ECG predict step of covariance matrix
-    This function uses a fixed a, b, evt and w
-    """
-    A = jacobian(*X, *a, *b, *evt, dt)
-    F = np.asmatrix(np.eye(17), dtype="float")
-    priori_p = A*P*A.T + F*Q*F.T
-    return priori_p
-
-
-def ecg_update(yk, X, P, R):
-    X = np.asmatrix(X, dtype="float").T
-    r = np.asmatrix(np.random.normal(0, 0.1, 2)).T
-
-    g = np.matrix(np.eye(2), dtype="float") * X + r
-
-    C = np.matrix(np.eye(2), dtype="float")
-    G = np.matrix([1, 1], dtype="float").T
-
-    s = C*P*C.T + G
-    K = P*C.T * s.I
-
-    posteri_x = X + K * (yk-g)
-    posteri_p = P - K*C*P
-    return (np.array(posteri_x.T)[0], posteri_p)
