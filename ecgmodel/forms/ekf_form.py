07dbe046ef53368ceef19d7fc9d3df41f0c9ecca
Andrew Au <andrew.ch.au@outlook.com>
2019-11-21 14:31:46 +1100

    add alternate ekf form

--- ecgmodel/forms/ekf_form.py
+++ ecgmodel/forms/ekf_form.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
+from numpy import array as nparr
+
 from PyQt5.QtCore import QRegExp, Qt
 from PyQt5.QtGui import QRegExpValidator
-from PyQt5.QtWidgets import (QDialog, QDialogButtonBox, QFormLayout,
-                             QGroupBox, QLineEdit, QVBoxLayout)
+from PyQt5.QtWidgets import (QDialog, QDialogButtonBox, QFormLayout, QGroupBox,
+                             QHBoxLayout, QLineEdit, QVBoxLayout)
 
 
 class KalmanFilterForm(QDialog):
@@ -11,23 +13,41 @@ class KalmanFilterForm(QDialog):
     Checkboxes for plot or step-by-step output
     Other potential values, such as noise
     """
-
     def __init__(self, parent=None):
         super().__init__()
 
         self.setWindowTitle("Parameter Estimation")
         self.setMinimumSize(550, 50)
 
-        varianceBox = QGroupBox("Kalman Filter options")
-        self.entry = QLineEdit("1")
-        self.entry.setFixedWidth(60)
-        self.entry.setMaxLength(5)
-        t_validator = QRegExpValidator(QRegExp(r"\d*"))
-        self.entry.setValidator(t_validator)
+        def make_entry(val, vdtr):
+            entry = QLineEdit(str(val))
+            entry.setFixedWidth(50)
+            entry.setMaxLength(4)
+            entry.setValidator(vdtr)
+            return entry
 
-        varianceLayout = QFormLayout(varianceBox)
-        varianceLayout.addRow("Initial uncertainty:", self.entry)
-        varianceBox.setLayout(varianceLayout)
+        entries_a = QHBoxLayout()
+        entries_b = QHBoxLayout()
+        entries_evt = QHBoxLayout()
+        num_validator = QRegExpValidator(QRegExp(r"\d*"))
+        for i in range(0, 5):
+            ai = make_entry(1, num_validator)
+            bi = make_entry(1, num_validator)
+            ei = make_entry(1, num_validator)
+
+            entries_a.addWidget(ai)
+            entries_b.addWidget(bi)
+            entries_evt.addWidget(ei)
+
+        entry_omega = make_entry(1, num_validator)
+
+        varianceFrame = QGroupBox("Kalman Filter uncertainty")
+        self.varianceForm = QFormLayout()
+        self.varianceForm.addRow("a", entries_a)
+        self.varianceForm.addRow("b", entries_b)
+        self.varianceForm.addRow("theta", entries_evt)
+        self.varianceForm.addRow("omega", entry_omega)
+        varianceFrame.setLayout(self.varianceForm)
 
         buttons = QDialogButtonBox(Qt.Horizontal, self)
         buttons.addButton(QDialogButtonBox.Ok)
@@ -35,16 +55,24 @@ class KalmanFilterForm(QDialog):
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
 
-        formlayout = QVBoxLayout(self)
-        formlayout.addWidget(varianceBox)
-        formlayout.addWidget(buttons)
+        frame = QVBoxLayout(self)
+        frame.addWidget(varianceFrame)
+        frame.addWidget(buttons)
 
     def get_variance(self):
-        try:
-            val = int(self.entry.text())
-        except:
-            val = 1
-        return 1 if not val else val
+        entries_a = self.varianceForm.itemAt(0, 1)
+        entries_b = self.varianceForm.itemAt(1, 1)
+        entries_evt = self.varianceForm.itemAt(2, 1)
+
+        a_ = [entries_a.itemAt(i).widget().text().strip() for i in range(5)]
+        b_ = [entries_b.itemAt(i).widget().text().strip() for i in range(5)]
+        evt_ = [entries_evt.itemAt(i).widget().text().strip() for i in range(5)]
+        omega_ = self.varianceForm.itemAt(3, 1).widget().text().strip()
+        a = [str(1) if not ai else int(ai) for ai in a_]
+        b = [str(1) if not bi else int(bi) for bi in b_]
+        evt = [str(1) if not ei else int(ei) for ei in evt_]
+        omega = str(1) if not omega_ else int(omega_)
+        return nparr([1, 1, 1, *a, *b, *evt, omega])
 
     @staticmethod
     def get_ekf_options(parent=None):
